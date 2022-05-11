c27202e89308212623f71f37bb8f4d0500069371
Thomas Chong <thomaschong0929@gmail.com>
2022-05-11 22:13:45 +0800

    modified reply title

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('!flip'):
-        await message.reply(embed=discord.Embed(title=random.choice(['先攻', '後攻']), description='OPEN!', color=discord.Color.blue()))
+        await message.reply(embed=discord.Embed(title=random.choice(['你係先攻', '你係後攻']), description='OPEN!', color=discord.Color.blue()))
 
 @tasks.loop(hours=1)
 async def keep_alive():

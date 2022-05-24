0da985df9bf811e4b530dbb00f40d070100a41c7
Thomas Chong <thomaschong0929@gmail.com>
2022-05-24 18:59:49 +0800

    added suzu description

--- main.py
+++ main.py
@@ -18,7 +18,14 @@ async def on_message(message):
         return
 
     if message.content.startswith('!flip'):
-        await message.reply(embed=discord.Embed(title=random.choice(['你係先攻', '你係後攻']), description='OPEN!', color=discord.Color.blue()))
+        title = random.choice(['你係先攻', '你係後攻'])
+        
+        if message.author.id == os.getenv('SUZU_DISCORD_ID'):
+            description = '鈴姐咁勁，點同你打deck'
+        else:
+            description = 'OPEN!'
+        
+        await message.reply(embed=discord.Embed(title=title, description=description, color=discord.Color.blue()))
 
 @tasks.loop(hours=1)
 async def keep_alive():

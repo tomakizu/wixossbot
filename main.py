3b582d7c569a173bf4cdc6eebc7b28a073feb599
Thomas Chong <thomaschong0929@gmail.com>
2022-08-08 22:24:01 +0800

    separated bada text for future development

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 client = discord.Client()
 
+def get_bada_text():
+    return '巴打 我留意左你好耐\n你真係好幽默\n我自問都算個ok幽默既人 但係你幽默感比我真係高十幾二十倍\n錯 應該係100倍先岩\n估唔到我係呢個post竟然見識到你既威力\n我真係笑到爆左肚 依家一路打字一路撿返d腸呀內臟呀腎呀\n塞返落個肚到\n你有冇諗過未來幾個月去紅館開棟篤笑?'
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -18,8 +22,7 @@ async def on_message(message):
         return
 
     if '新手' in message.content and discord.utils.get(message.guild.roles, name='唔係新手') in message.author.roles:
-        text = '巴打 我留意左你好耐\n你真係好幽默\n我自問都算個ok幽默既人 但係你幽默感比我真係高十幾二十倍\n錯 應該係100倍先岩\n估唔到我係呢個post竟然見識到你既威力\n我真係笑到爆左肚 依家一路打字一路撿返d腸呀內臟呀腎呀\n塞返落個肚到\n你有冇諗過未來幾個月去紅館開棟篤笑?'
-        await message.reply(text)
+        await message.reply(get_bada_text())
 
     if message.content.startswith('!flip') or message.content.startswith('！flip'):
         title = random.choice(['你係先攻', '你係後攻'])
